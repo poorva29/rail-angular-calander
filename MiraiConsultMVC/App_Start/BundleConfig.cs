f0a2f065eb0d1eddbc1fa7373eae7b45bfde12b7
Yashwant Singh Rathore <yashwant@vertisinfotech.com>
2014-07-16 15:50:11 +0530

    Added AssignQuestion.cshtml file

--- MiraiConsultMVC/App_Start/BundleConfig.cs
+++ MiraiConsultMVC/App_Start/BundleConfig.cs
@@ -18,6 +18,15 @@ namespace MiraiConsultMVC
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/assignquestionjs").Include(
+                     "~/Content/chosen/chosen.jquery.js",
+                     "~/Content/js/jquery.dataTables.js",
+                     "~/Content/js/bootstrap-pagination.js"
+                    ));
+
+             bundles.Add(new ScriptBundle("~/bundles/assignquestioncss").Include(
+                     "~/Content/chosen/chosen.css"
+                    ));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(

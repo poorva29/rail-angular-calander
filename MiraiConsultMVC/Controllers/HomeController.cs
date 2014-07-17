fbab67a35d319547c17740e04c1964291b32844b
Priyanka Priyadarshni <priyanka.p@vertisinfotech.com>
2014-07-17 12:50:59 +0530

    Completed mira-445

--- MiraiConsultMVC/Controllers/HomeController.cs
+++ MiraiConsultMVC/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MiraiConsultMVC.Models.Home;
+using System.Configuration;
+using MiraiConsultMVC.Models;
 
 namespace MiraiConsultMVC.Controllers
 {
@@ -24,8 +27,59 @@ namespace MiraiConsultMVC.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
+            return View();
+        }
 
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        public ActionResult TermsOfUse()
+        {
+            return View();
+        }
+
+        public ActionResult HowWeWorks()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(Contact contact)
+        {
+            ViewBag.Message = "";
+            if (ModelState.IsValid)
+            {
+                string from_address = null;
+                bool sent_mail = false;
+                string name;
+                string msgBody = contact.message;
+                string subject = "Askmirai Contact Us";
+                string emailId = Convert.ToString(ConfigurationManager.AppSettings["ContactUSEmail"].ToString().Trim());
+                if (Session["UserId"] != null && Session["UserEmail"] != null)
+                {
+                    from_address = Convert.ToString(Session["UserEmail"]);
+                    name = Convert.ToString(Session["UserFullName"]);
+                }
+                else
+                {
+                    from_address = contact.Email;
+                    name = contact.Name;
+                }
+                msgBody = "<b>Sender Name:</b> " + name + "<br> <b>Sender Email:</b> " + from_address + "<br><b> Message: </b>" + msgBody;
+                sent_mail = Mail.SendHTMLMail(from_address, emailId, subject, msgBody, null);
+                if (!sent_mail)
+                {
+                    ViewBag.Message = "Failed to send email.";
+                }
+                else
+                {
+                    ViewBag.Message = "Thank you for contacting us.";                    
+                    ModelState.Clear();
+                }
+            }
             return View();
         }
     }
--- MiraiConsultMVC/Utilities/Mail.cs
+++ MiraiConsultMVC/Utilities/Mail.cs
@@ -18,7 +18,7 @@ using System.Net;
 using System.Collections.Generic;
 using log4net;
 
-namespace askmirai
+namespace MiraiConsultMVC
 {
     /// <summary>
     /// send mail methods.

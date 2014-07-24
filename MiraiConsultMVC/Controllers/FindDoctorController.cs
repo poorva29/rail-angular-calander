fb83f83742686da9eb435484b4f6d45fa88d3a87
Pavan <pavan.shevle@vertisinfotech.com>
2014-07-24 15:18:06 +0530

    Model, view ,controllers created.

---
+++ MiraiConsultMVC/Controllers/FindDoctorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiraiConsultMVC.Controllers
+{
+    public class FindDoctorController : Controller
+    {
+        //
+        // GET: /Find/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FindDoctors()
+        {
+
+
+            return View();
+        }
+
+    }
+}

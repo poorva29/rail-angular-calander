a528a79bba7389a8ff09517da668335b7ec16020
Pavan <pavan.shevle@vertisinfotech.com>
2014-07-24 17:18:39 +0530

    Find doctor page model ,view ,controller is completed

--- MiraiConsultMVC/Controllers/FindDoctorController.cs
+++ MiraiConsultMVC/Controllers/FindDoctorController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MiraiConsultMVC.Models;
+using System.Configuration;
 
 namespace MiraiConsultMVC.Controllers
 {
@@ -15,11 +17,25 @@ namespace MiraiConsultMVC.Controllers
         {
             return View();
         }
-        public ActionResult FindDoctors()
+        [HttpGet]
+        public ActionResult FindDoctors(FindDoctorModel findDoctorModel)
         {
-
-
-            return View();
+            if (Session["UserId"] != null && Session["UserType"] != null)
+                findDoctorModel.HiddenUserType = Convert.ToString(Session["UserType"]);
+            else
+                findDoctorModel.HiddenUserType = "-1";
+            if (Session["locationid"] != null && Session["cityid"] != null)
+            {
+                findDoctorModel.hdnCityId = Convert.ToInt32(Session["cityid"]) != 0 ? Convert.ToString(Session["cityid"]) : null;
+                findDoctorModel.hdnLoactionId = Convert.ToInt32(Session["locationid"]) != 0 ? Convert.ToString(Session["locationid"]) : null;
+            }
+            else
+            {
+                findDoctorModel.hdnCityId = null;
+                findDoctorModel.hdnLoactionId = null;
+            }
+           findDoctorModel.hdnDocconnecturl = Convert.ToString(ConfigurationSettings.AppSettings["DocConnectApptUrl"]);
+           return View(findDoctorModel);
         }
 
     }
--- MiraiConsultMVC/Models/FindDoctorModel.cs
+++ MiraiConsultMVC/Models/FindDoctorModel.cs
@@ -7,6 +7,9 @@ namespace MiraiConsultMVC.Models
 {
     public class FindDoctorModel
     {
-
+       public string HiddenUserType { get; set; }
+       public string hdnCityId { get; set; }
+       public string hdnLoactionId { get; set; }
+       public string hdnDocconnecturl { get; set; }
     }
 }

c26661d9bbcddfa3232157c1ba569a56691d24dd
Pavan <pavan.shevle@vertisinfotech.com>
2014-08-12 16:01:37 +0530

    HttpUtility.UrlDecode() is removed

--- MiraiConsultMVC/Controllers/PatientsController.cs
+++ MiraiConsultMVC/Controllers/PatientsController.cs
@@ -426,7 +426,6 @@ namespace MiraiConsultMVC.Controllers
         [HttpGet]
         public ActionResult GetSimilarQuestion(string questionText)
         {
-            questionText= HttpUtility.UrlDecode(questionText);   
             IList<AskDoctor> lstQuestions = new List<AskDoctor>();
             var questionList = db.get_AllQuestionsByTag(questionText, Convert.ToInt32(QuestionStatus.Approved)).ToList();
             if (questionList != null && questionList.Count() > 0)
@@ -446,7 +445,6 @@ namespace MiraiConsultMVC.Controllers
         [HttpGet]
         public JsonResult question_Insert(string questionText)
         {
-            questionText = HttpUtility.UrlDecode(questionText); 
             int userId = 0;
             int result = 0;
             if (Session["UserId"] != null && Session["UserType"] != null)

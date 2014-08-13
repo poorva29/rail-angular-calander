9a1262a02d87e0a4f09c3cb1efc73cc52b362dd3
Ravindra <ravindra.kale@vertisinfotech.com>
2014-08-13 18:38:43 +0530

    Remove unnecessary code

--- MiraiConsultMVC/Content/js/utility.js
+++ MiraiConsultMVC/Content/js/utility.js
@@ -86,28 +86,6 @@ function thankToDoctor(userid, answerid, button, lastname, mobileno, emailid, qu
     });
     return false;
 }
-//function isEnterPress(evt, x) {
-//    var charCode = (evt.which) ? evt.which : event.keyCode
-//    if (charCode == 13) {
-//        var session = '<%=Session["userType"]%>';
-//        if (session != null && session == 2)
-//            window.location.href = "../Patients/AskDoctor?question=" + x.value;
-//        else
-//            window.location.href = "../Patients/similarQuestions?question=" + x.value;
-//        return false;
-//    }
-//    return true;
-//}
-
-        var session = '<%=Session["userType"]%>';
-        if (session != null && session == 2)
-            window.location.href = "/similarQuestions/" + x.value;
-        else
-            window.location.href = "/similarQuestions/" + x.value;
-        return false;
-    }
-    return true;
-}
 
 function MinLength(sender, args) {
     args.IsValid = (args.Value.length >= 6);

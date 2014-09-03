60bcc8c6f2ad25e6fb3a64bfda7b3b9d4e7cced0
Ravindra <ravindra.kale@vertisinfotech.com>
2014-09-03 12:13:28 +0530

    fixed Topic name in lower case

--- MiraiConsultMVC/Controllers/HomeController.cs
+++ MiraiConsultMVC/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace MiraiConsultMVC.Controllers
                 ListTags = UtilityManager.getInstance().get_allTagsWithCountOfAnsweredQuestions().Tables[0].AsEnumerable().Select(dataRow => new Tag
                 {
                     Text = dataRow.Field<string>("tagname"),
-                    NavigateUrl = "/topics/" + dataRow.Field<string>("tagname").Replace(' ', '-'),
+                    NavigateUrl = ("/topics/" + dataRow.Field<string>("tagname").Replace(' ', '-')).ToLower(),
                     TagWeight = Convert.ToInt32(dataRow.Field<string>("counts")),
                     ToolTip = dataRow.Field<string>("tagname")
 
@@ -214,7 +214,7 @@ namespace MiraiConsultMVC.Controllers
             ListTags = assignCssToTags(UtilityManager.getInstance().get_allTagsWithCountOfAnsweredQuestions().Tables[0].AsEnumerable().Select(dataRow => new Tag
             {
                 Text = dataRow.Field<string>("tagname"),
-                NavigateUrl = "/topics/" + dataRow.Field<string>("tagname").Replace(' ','-'),
+                NavigateUrl = ("/topics/" + dataRow.Field<string>("tagname").Replace(' ', '-')).ToLower(),
                 TagWeight = Convert.ToInt32(dataRow.Field<string>("counts")),
                 ToolTip = dataRow.Field<string>("tagname")
             }).OrderBy(x => x.Text).ToList());

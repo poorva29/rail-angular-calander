60631e9628079be9b7e52f16eb07961aaeaf37a0
Priyanka Priyadarshni <priyanka.p@vertisinfotech.com>
2014-09-04 18:20:40 +0530

    Crete table with primary key

--- migrationsqls/mira-739/Create_pre_registration.sql
+++ migrationsqls/mira-739/Create_pre_registration.sql
@@ -1,5 +1,5 @@
 
-/****** Object:  Table [dbo].[preregistration]    Script Date: 9/4/2014 4:51:31 PM ******/
+/****** Object:  Table [dbo].[pre_registration]    Script Date: 9/4/2014 6:19:35 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,7 +9,8 @@ GO
 SET ANSI_PADDING ON
 GO
 
-CREATE TABLE [dbo].[preregistration](
+CREATE TABLE [dbo].[pre_registration](
+	[prereguserid] [int] NOT NULL,
 	[firstname] [varchar](50) NOT NULL,
 	[lastname] [varchar](50) NOT NULL,
 	[email] [varchar](50) NOT NULL,

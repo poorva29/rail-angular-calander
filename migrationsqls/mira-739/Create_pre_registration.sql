a40e3b2a9f2234af28164508a9ac0cb2a6325151
Priyanka Priyadarshni <priyanka.p@vertisinfotech.com>
2014-09-04 18:30:32 +0530

    Creted table

--- migrationsqls/mira-739/Create_pre_registration.sql
+++ migrationsqls/mira-739/Create_pre_registration.sql
@@ -1,5 +1,5 @@
 
-/****** Object:  Table [dbo].[pre_registration]    Script Date: 9/4/2014 6:19:35 PM ******/
+/****** Object:  Table [dbo].[tbl_pre_registration]    Script Date: 9/4/2014 6:30:13 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,12 +9,16 @@ GO
 SET ANSI_PADDING ON
 GO
 
-CREATE TABLE [dbo].[pre_registration](
-	[prereguserid] [int] NOT NULL,
+CREATE TABLE [dbo].[tbl_pre_registration](
+	[prereg_userid] [int] IDENTITY(1,1) NOT NULL,
 	[firstname] [varchar](50) NOT NULL,
 	[lastname] [varchar](50) NOT NULL,
-	[email] [varchar](50) NOT NULL,
-	[mobileno] [varchar](15) NOT NULL
+	[email] [varbinary](50) NOT NULL,
+	[mobileno] [varchar](15) NOT NULL,
+ CONSTRAINT [PK_tbl_pre_registration] PRIMARY KEY CLUSTERED 
+(
+	[prereg_userid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO

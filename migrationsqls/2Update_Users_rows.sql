588c47f39275d15683da26908d5fc25823f7b228
Pavan <pavan.shevle@vertisinfotech.com>
2014-09-12 15:13:47 +0530

    sql author,date ,description is added

--- migrationsqls/2Update_Users_rows.sql
+++ migrationsqls/2Update_Users_rows.sql
@@ -1,6 +1,8 @@
-USE [AsquareMirai]
-GO
-
+-- =============================================
+-- Author:		Pavan Shevle
+-- Create date: 12/09/2014
+-- Alter Log: Added name_seo
+-- =============================================
   UPDATE [dbo].[users]
   SET 
   [name_seo] =LOWER(dbo.RemoveSpecialChars(REPLACE(LTRIM(RTRIM(firstname)) + '-' +LTRIM(RTRIM(lastname)), ' ', '-')))

da9ebb5dfcdba60b2e242731436b9e510e29bee5
Poorva Mahajan <poorva.mahajan2990@gmail.com>
2015-07-06 20:11:23 +0530

    Added two calanders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Training::Application.config.secret_key_base = 'ad7420a8689b7468fb90bb3b70001aaf36ad6f6d2e1453c14c40a1d3d905d7dd1fc0dbafac50372d8c3c4853aea1fee8f6a6697a385956045e7adf95e1984fff'

0afc500d8429fa30755e9b106d44228546b8319e
Poorva Mahajan <poorva.mahajan2990@gmail.com>
2015-07-09 19:48:47 +0530

    Added modal for calender select event

--- public/javascript/calender/calendarDemo.js
+++ public/javascript/calender/calendarDemo.js
@@ -1,32 +1,41 @@
-angular.module('calendarDemoApp',['ui.calendar', 'ui.bootstrap']).controller('CalendarCtrl',function($scope,$compile,uiCalendarConfig) {
-  var date = new Date();
-  var d = date.getDate();
-  var m = date.getMonth();
-  var y = date.getFullYear();
+angular.module('calendarDemoApp',['ui.calendar', 'ui.bootstrap'])
+  .controller('CalendarCtrl',function($scope, $modal, $log) {
+    // Calendar specific changes !
 
-  $scope.alertOnEventClick = function( date, jsEvent, view){
-    $scope.alertMessage = (date.title + ' was clicked ');
-  };
-  /* alert on Drop */
-  $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
-    $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
-  };
-  /* alert on Resize */
-  $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
-    $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
-  };
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
 
-  $scope.events = [
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),stick: true},
+    $scope.alertOnEventClick = function(event, jsEvent, view){
+      // $scope.alertMessage = (event.title + ' was clicked ');
+    };
+    /* alert on Drop */
+    $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
+      // $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
+    };
+    /* alert on Resize */
+    $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
+      // $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+    };
+
+    $scope.slotSelected = function(start, end, jsEvent, view){
+      // start.format('hh:mm') , start.hours()
+      console.log(start);
+      $scope.open();
+    }
+
+    $scope.events = [
+      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 0),stick: true},
     ];
 
-  $scope.uiConfig = {
+    $scope.uiConfig = {
       calendar:{
         defaultView: 'agendaWeek',
-        height: 500,
+        height: 550,
         editable: true,
         header:{
-          left: 'agendaDay agendaWeek',
+          left: 'agendaDay agendaWeek month',
           center: 'title',
           right: 'today prev,next'
         },
@@ -35,15 +44,68 @@ angular.module('calendarDemoApp',['ui.calendar', 'ui.bootstrap']).controller('Ca
             titleFormat: 'YYYY, MM, DD',
           }
         },
-        slotDuration: '00:45:01',
+        slotDuration: '01:00:00',
         allDaySlot: false,
         selectable: true,
         eventClick: $scope.alertOnEventClick,
         eventDrop: $scope.alertOnDrop,
         eventResize: $scope.alertOnResize,
-        eventRender: $scope.eventRender
+        eventRender: $scope.eventRender,
+        select: $scope.slotSelected,
       }
     };
+    $scope.eventSources = [$scope.events];
 
-  $scope.eventSources = [$scope.events];
-});
+    // Modal specific changes !
+
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.animationsEnabled = true;
+
+    $scope.open = function (size) {
+
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+        console.log(selectedItem);
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $scope.toggleAnimation = function () {
+      $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+
+  });
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+angular.module('calendarDemoApp')
+  .controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+    $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  });

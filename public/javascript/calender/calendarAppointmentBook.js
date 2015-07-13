f7c0d34227107648203cea603b4636903081843b
Poorva Mahajan <poorva.mahajan2990@gmail.com>
2015-07-13 18:52:27 +0530

    Added functionalily for stopping event overlapping

--- public/javascript/calender/calendarAppointmentBook.js
+++ public/javascript/calender/calendarAppointmentBook.js
@@ -1,5 +1,5 @@
-angular.module('BookAppointmentApp',['ui.calendar', 'ui.bootstrap', 'angular-underscore'])
-  .controller('BookAppointmentCtrl',function($scope, $modal, $log) {
+angular.module('BookAppointmentApp',['ui.calendar', 'ui.bootstrap', 'angular-underscore', 'flash'])
+  .controller('BookAppointmentCtrl',function($scope, $modal, $log, Flash) {
     /* Calendar specific changes
       This has calendar configurations and event binding for the calendar
     */
@@ -22,9 +22,28 @@ angular.module('BookAppointmentApp',['ui.calendar', 'ui.bootstrap', 'angular-und
       // $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
     };
 
+    $scope.stopEventOverloap = function(start, end){
+      for(i in $scope.events){
+        if (end > $scope.events[i].start && start < $scope.events[i].end){
+          return true;
+        }
+      }
+      return false;
+    };
+
+    $scope.showAlert = function (type, message) {
+      Flash.create(type, message);
+    }
+
     $scope.slotSelected = function(start, end, jsEvent, view){
       // start.format('hh:mm') , start.hours()
-      $scope.open(start, end, jsEvent, view, '');
+        if($scope.stopEventOverloap(start, end)){
+          var message = '<strong> Not Booked !</strong>  Appointment Booked For Selected Time.';
+          $scope.showAlert('danger', message);
+          $('#appointmentBookingCalendar').fullCalendar('unselect');
+        }else{
+          $scope.open(start, end, jsEvent, view, '');
+        }
     };
 
     $scope.generateUniqueEventId = function(start_date){
@@ -35,8 +54,8 @@ angular.module('BookAppointmentApp',['ui.calendar', 'ui.bootstrap', 'angular-und
       {
         id: $scope.generateUniqueEventId(moment(new Date(y, m, d + 1))),
         title: 'Birthday Party',
-        start: new Date(y, m, d + 1, 19, 0),
-        end: new Date(y, m, d + 1, 22, 0),
+        start: moment.utc(new Date(y, m, d + 1, 19, 30)),
+        end: moment.utc(new Date(y, m, d + 1, 22, 30)),
         stick: true
       },
     ];
@@ -59,11 +78,12 @@ angular.module('BookAppointmentApp',['ui.calendar', 'ui.bootstrap', 'angular-und
         slotDuration: '01:00:00',
         allDaySlot: false,
         selectable: true,
+        selectOverlap: false,
         eventClick: $scope.alertOnEventClick,
         eventDrop: $scope.alertOnDrop,
         eventResize: $scope.alertOnResize,
         eventRender: $scope.eventRender,
-        select: $scope.slotSelected,
+        select: $scope.slotSelected
       }
     };
     $scope.eventSources = [$scope.events];
@@ -97,16 +117,22 @@ angular.module('BookAppointmentApp',['ui.calendar', 'ui.bootstrap', 'angular-und
         }
       });
 
-      modalInstance.result.then(function (selectedItem) {
-        $scope.selected_event = selectedItem;
+      $scope.addEvent= function(){
         $scope.events.push({
-          id: $scope.generateUniqueEventId(selectedItem.start),
+          id: $scope.generateUniqueEventId($scope.selected_event.start),
           title: 'Open Sesame',
           start: $scope.selected_event.start,
           end: $scope.selected_event.end,
           className: ['openSesame'],
           stick: true
         });
+      };
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected_event = selectedItem;
+        $scope.addEvent();
+        var message = '<strong> Booked !</strong>  Appointment Created Successfully.';
+        $scope.showAlert('success', message);
       }, function () {
         $log.info('Modal dismissed at: ' + new Date());
       });
@@ -143,6 +169,4 @@ angular.module('BookAppointmentApp',['ui.calendar', 'ui.bootstrap', 'angular-und
         $log.info('Modal dismissed at: ' + new Date());
       });
     };
-
-
   });
